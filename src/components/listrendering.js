a0dfa3d3b50d278c39b794ceb48689be12baa6d6
Pushpendra Vishwakarma <48829314+Pushpendrahpx@users.noreply.github.com>
2019-11-02 01:46:23 +0530

    Understanding Map, filter

--- src/components/listrendering.js
+++ src/components/listrendering.js
@@ -1,16 +1,37 @@
-import React,{ Component } from "react";
-class Listrendering extends Component{
-    constructor(props){
-        super(props)
+import React from 'react';
 
-    }
-
-    render(){
-        return(
-            <div className='jumbotron'>
-                List Rendering Component
+function listrendering(props) {
+    const names = ["Pushpendra","Vinod","Shiven"];
+    const person = [{
+        id:1,
+        name:'Pushpendra',
+        age:18,
+        skill:'ANgular'
+    },{
+        id:2,
+        name:'Vinod',
+        age:19,
+        skill:'C++ Libraries'
+    },{
+        id:3,
+        name:'Shiven',
+        age:18,
+        skill:'React'
+    }];
+    const data = person.map((person)=>(
+        <div className=' container'>
+            <div className='card'>
+            <div className='card-header'>{person.name}</div>
+            <div className='card-body'>I have done specialization in {person.skill} and I'm {person.age} years old.</div>
+        
             </div>
-        );
-    }
+          </div>
+    ));
+    return (
+        <div>
+          {data}
+        </div>
+    );
 }
-export default Listrendering;
+
+export default listrendering;
