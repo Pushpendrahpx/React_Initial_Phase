373c4cddb73dc638e11d8128ef6432d67a62cb64
Pushpendra Vishwakarma <48829314+Pushpendrahpx@users.noreply.github.com>
2019-11-04 21:59:46 +0530

    Changed Code in Form1.js
    Initially There was warning in Onchange event, But now solvedm changed - event.target.value

--- src/components/forms/form1.js
+++ src/components/forms/form1.js
@@ -5,29 +5,53 @@ class form1 extends Component {
         super(props)
 
         this.state = {
-                 
+                email:'',
+                password:''
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangepass = this.handleChangepass.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleChange(event){
+        this.setState({
+            email:event.target.value
+        });
+    }
+    handleChangepass(event){
+        this.setState({
+            password:event.target.value
+        });
+    }
+    handleSubmit(event) {
+        alert('A emails was submitted: ' + this.state.email);
+        event.preventDefault();
+      }
+    
     render() {
         return (
             <div className='container'>
                 <div className='jumbotron'>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className='form-group'>
                 
                           <div className='lead'>Registration Forms : Model 1</div>
                         </div>
                         <div className='form-group'>
                             <label>Email ID - </label>
-                            <input type='email' placeholder='EMAIL ID' className='form-control' />
+                            <input type='email'
+                             value={this.state.email}
+                             onChange={this.handleChange} placeholder='EMAIL ID' className='form-control' />
                         </div>
                         <div className='form-group'>
                             <label>Password - </label>
-                            <input type='password' placeholder='Password' className='form-control' />
+                            <input type='password' 
+                            value={this.state.password} 
+                            onChange={this.handleChangepass} placeholder='Password' className='form-control' />
                         </div>
                         <div className='form-group'>
-                            <button type='button' className='btn btn-danger btn-large large '>Submit</button>
+                            <button type='submit' className='btn btn-danger btn-large large '>Submit</button>
                         </div>
                     </form>
                 </div>
